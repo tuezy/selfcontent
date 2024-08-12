cc079a4083566e469b5ff0c5231fd0a1f1b7ebfc
Tu <9281384+vohoangtuarchives@users.noreply.github.com>
2024-08-12 18:08:27 +0700

    hex

--- app/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Route;
-Route::get('/', function(){
-    $about = Artisan::call('app:test-art');
-    return $about;
-});
--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
 
-use Illuminate\Http\Request;
-
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions as ConfigurationException;
-use Exception as BaseException;
-use Illuminate\Foundation\Configuration\Middleware;
-use Symfony\Component\Console\Input\ArgvInput;
-
-require_once __DIR__.'/vendor/autoload_runtime.php';
-
-return function (array $context): void {
-    $app =  \Kernel\App::init($context)
-        ->create()
-        ->useBootstrapPath(storage_path("bootstrap"));
-    $app->runningInConsole() ? exit( $app->handleCommand(new ArgvInput)): $app ->handleRequest(Request::capture());
-};
+phpinfo();
